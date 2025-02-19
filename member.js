df44c3346363a5ce68c7884f6dc598a3fefe4c44
Abhishank Dubey <dubey.abhishank@gmail.com>
2025-02-19 11:08:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    addSkill: function(skill) {
+      console.log('Adding skill:', skill)
+    },
+    removeSkill: function(skill) {
+      console.log('Removing skill:', skill)
+    }
+  }
+}
